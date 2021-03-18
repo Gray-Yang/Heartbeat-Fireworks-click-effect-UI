b9c1fe67bb89e8ad76bcf0fe646a4a9f3997ff52
Gray-Yang <59634081+Gray-Yang@users.noreply.github.com>
2021-03-17 23:44:24 -0400

    Add files via upload

---
+++ fireworks.js
@@ -0,0 +1,8 @@
+<!-- 鼠标点击效果 -->
+<script src="https://blog-static.cnblogs.com/files/e-cat/cursor-effects.js"></script>
+
+
+<!-- 鼠标点击烟花爆炸效果 -->
+<canvas class="fireworks" style="position: fixed;left: 0;top: 0;z-index: 1; pointer-events: none;"></canvas>
+<script type="text/javascript" src="//cdn.bootcss.com/animejs/2.2.0/anime.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/TRHX/CDN-for-love109.cn@1.7/js/fireworks.js"></script>
